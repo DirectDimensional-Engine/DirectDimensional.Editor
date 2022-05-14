15e0205f9e3d72d7933e6bb154423cc58a360b6a
ItsRealGaming <DestinyHero789@gmail.com>
2022-05-14 15:49:44 +0700

    remove testing TextWrapped API

--- EditorLifecycle.cs
+++ EditorLifecycle.cs
@@ -3,24 +3,14 @@ using DirectDimensional.Editor.GUI;
 using DirectDimensional.Core;
 using System.Runtime.InteropServices;
 using System.Text;
-using DirectDimensional.Core.Utilities;
 
 using static StbTrueTypeSharp.StbTrueType;
 
 namespace DirectDimensional.Editor {
     internal unsafe static class EditorLifecycle {
-        private static HorizontalTextAnchor hanchor = HorizontalTextAnchor.Left;
-        private static VerticalTextAnchor vanchor = VerticalTextAnchor.Top;
-
         public static void Initialize() {
             ImGuiEngine.Initialize();
-
-            KeyboardAxisRegister.Register(0, KeyboardCode.Left, KeyboardCode.Right);
-            KeyboardAxisRegister.Register(1, KeyboardCode.Down, KeyboardCode.Up);
         }
-
-        private static int repeat = 0;
-        private static float height = 300;
 
         public static void Cycle() {
             ImGuiEngine.NewFrame();
